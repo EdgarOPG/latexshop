f9dc5285c74b1db787d40ef362d2dd11de8d538a
EdgarOPG <eopg95@gmail.com>
2016-05-13 16:22:01 -0600

    s

--- db/playeras.php
+++ db/playeras.php
@@ -1,6 +1,8 @@
 <?php
 
+$conexion = mysqli_connect("localhost","root","1234","latexshop") or die("Error " . mysqli_error($conexion));// select the db name
 
+$query = "SELECT * FROM playeras" or die("Error in the consult.." . mysqli_error($conexion)); 
 
 $result = mysqli_query($conexion, $query);
 
